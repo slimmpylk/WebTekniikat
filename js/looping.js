a287f088977ced0299deb5171830550634f9a068
Samuli Pylkkönen <samuli.pylkkonen@gmail.com>
2022-03-31 17:06:39 +0300

    first commit

---
+++ js/looping.js
@@ -0,0 +1,29 @@
+/* 
+    Created on : May, 2019 
+    Author     : Liisa Auer, Oulu University of Applied Sciences
+    Licence    : CC-BY-4.0
+*/
+
+/*
+ * Student name :
+ * Started date :
+ * Ended   date :
+ * 
+ */
+
+// define here the common functions calculateHedgeRow and getMeters
+
+
+
+function lt01() {
+
+}
+
+
+function lt02() {
+
+}
+
+function lt03() {    
+
+}
